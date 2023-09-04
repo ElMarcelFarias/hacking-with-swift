f3541561e25e92c7535f2102898cd08bd9ea0c4e
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-09-04 15:16:54 -0300

    Day: 001 finalizado!

--- hacking-001.playground/Contents.swift
+++ hacking-001.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Criando var do tipo String
+var str = "Hello, word!"
+
+//Mudando valor da variavel
+str = "GoodBye"
+
+//Criando variavel do tipo Inteiro
+var age = 28
+var population = 8_000_000
+
+//String com multiplas linhas
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+//Criando variavel do tipo Double
+var pi = 3.141
+
+//Criando variavel do tipo boolean
+var awesome = true
+
+//Interpolação de Strings
+var score = 85
+var str3 = "The test results are here: \(score)"
+var results = "The results are here: \(str3)"
+
+let taylor = "Swift"
+
+//Tipando a variavel antes de atribuir seu valor.
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks = true
