3a529f02b25ac2a889b1f679880eaa723ba59968
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-09-08 19:40:25 -0300

    Change: Finalizando Day 03 - Hacking with swift

--- hacking-003.playground/Contents.swift
+++ hacking-003.playground/Contents.swift
@@ -64,3 +64,42 @@ if firstCard + secondCard == 2 {
 } else {
     print("Regular cards")
 }
+
+
+if age1 > 18 || age2 > 18 {
+    print("At least one is over 18")
+}
+
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+let firstCard = 11
+let secondCard = 10
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+
+let weather = "sunny"
+
+switch weather {
+    case "rain":
+        print("Bring an umbrella")
+    case "snow":
+        print("Wrap up warm")
+    case "sunny":
+        print("Wear sunscreen")
+        fallthrough
+    default:
+        print("Enjoy your day!")
+}
+
+let score = 85
+
+switch score {
+    case 0..<50:
+        print("You failed badly.")
+    case 50..<85:
+        print("You did OK.")
+    default:
+        print("You did great!")
+}
