962571da1349b42ffaf7b0cf189fbb7c2831358e
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-10-02 21:08:50 -0300

    New: finalizando projeto 1 - parte two day 17 - Hacking with Swift

--- Projeto1/Projeto1/ViewController.swift
+++ Projeto1/Projeto1/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UITableViewController {
     var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Storm Viewer"
         // Do any additional setup after loading the view.
+        navigationController?.navigationBar.prefersLargeTitles = true
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -35,6 +38,13 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Imagem", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detalhe") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 
 
