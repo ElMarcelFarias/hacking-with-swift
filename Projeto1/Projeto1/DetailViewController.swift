5f02d68bbd085f6d678c3163d3f463279f47a37d
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-10-18 15:46:21 -0300

    New: Incluindo o nome da foto ao compartilhar!

--- Projeto1/Projeto1/DetailViewController.swift
+++ Projeto1/Projeto1/DetailViewController.swift
@@ -39,8 +39,12 @@ class DetailViewController: UIViewController {
             print("No image found")
             return
         }
+        
+        guard let nameImage = selectedImage else {
+            return
+        }
 
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        let vc = UIActivityViewController(activityItems: [image, nameImage], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
